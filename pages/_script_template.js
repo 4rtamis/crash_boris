4cf59a3c61bf733e7acce9bb33b8695f810a0e8c
= <=>
2022-03-24 12:04:39 +0100

    create and fill global script js

--- pages/_script_template.js
+++ pages/_script_template.js
@@ -2,32 +2,4 @@
 if (!localStorage.getItem("moteur")){
     localStorage.setItem("moteur",true);
 }
-
 document.getElementById("moteur").classList.add("active");
-
-//verify all the items and fill the inventory
-var parts = new Map();
-parts.set("moteur",["MO","Moteur"]);
-parts.set("réacteur",["RE","Réacteur"]);
-
-check = true
-for (var [key, value] of parts) {
-    if (!localStorage.getItem(key)){
-        check = false;
-        const div = document.getElementById(key);
-        div.classList.add("unknown-item");
-        const h3 = document.getElementById(key+"_title");
-        h3.textContent = "?";
-        const p = document.getElementById(key+"_p");
-        p.textContent = "";
-    } else {
-        document.getElementById(key).classList.add("found-item");
-        const h3 = document.getElementById(key+"_title");
-        h3.textContent = value[0];
-        const p = document.getElementById(key+"_p");
-        p.textContent = value[1];
-    }
-};
-if (check){
-    window.location = 'https://crashboris.h.minet.net/win.html';
-}
